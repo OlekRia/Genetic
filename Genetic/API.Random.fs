1fbc5be21459bd63ca2ff3433366c62b8cc7c254
oriabukha <alerya@gmail.com>
2023-07-19 13:12:38 +0200

    Initial structure.

---
+++ Genetic/API.Random.fs
@@ -0,0 +1,7 @@
+﻿module Genetic.API.Random
+
+let random toValue =
+    let r = new System.Random()
+    r.Next(0, toValue)
+
+let randomFrom1 x = 1 + random (x - 1)
---
+++ Genetic/Program.fs
@@ -0,0 +1,42 @@
+﻿module Genetic.Program
+
+let evaluate  = List.sortByDescending List.sum
+
+let selection (population: int list list): (int list * int list) list  =
+    let rec loop acc = function
+    | x::y::rest -> loop ((x, y)::acc) rest
+    | _ -> List.rev acc
+
+    loop [] population
+
+let crossover random =
+     List.map
+        (fun (a, b) ->
+            let splitAt = min a b |> List.length |> random |> List.splitAt
+            let (x1,y1), (x2,y2) = splitAt a, splitAt b
+            let res = (x1 @ y2), (y1 @ x2)
+            res)
+     >> List.collect (function (list1, list2) -> [list1; list2])
+
+
+
+let rec algorithm population =
+    let best =  List.maxBy List.sum population
+    printfn $"Current Best: [{List.sum best}] {best}"
+    
+    if (List.sum best) > 31 then
+        printf $"YO: {List.sum best}"
+        best
+    else
+        population
+        |> evaluate
+        |> selection
+        |> (crossover Genetic.API.Random.random)
+        |> algorithm
+
+
+let test = [[10; 11; 3]; [7; 8; 9]; [4; 5; 6]; [1; 2; 12]]
+algorithm test
+
+
+
